bdb0662a4a84192a60c678dca92caeee8513e659
Praveen Kumar Gupta <pg23403@gmail.com>
2024-12-14 01:17:31 +0530

    Few update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8">
+    <footer className="bg-gray-800 mt-8 text-white py-8">
       <div className="container mx-auto px-4 md:px-8 grid grid-cols-1 md:grid-cols-4 gap-8">
         {/* Name and Tagline */}
         <div>
